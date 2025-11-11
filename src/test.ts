2343896641a488f7a723c45175c7dab48fba4990
wgzhi <wuguozhi@baoxiaohe.com>
2025-11-11 13:51:13 +0800

    test

--- src/test.ts
+++ src/test.ts
@@ -3,3 +3,9 @@ import process from 'node:process'
 console.log(process.env)
 
 console.log(process.env.ALIST_SPACE_KEY)
+
+if(process.env.ALIST_SPACE_KEY) {
+    console.log(process.env.ALIST_SPACE_KEY.length)
+} else {
+    console.log('ALIST_SPACE_KEY is not set')
+}
